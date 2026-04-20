bd661a4d6757a4474811cac25d4f81b17ce8b4ea
Boltz333 <boltz333yt@gmail.com>
2026-04-20 19:49:58 +0200

    Re moddeld the entire shit to accually make a bit of fucking sense

--- control.gd
+++ control.gd
@@ -1,55 +1,67 @@
 extends Control
 
-var bean = 0
+var score_bean = 0
+var bean_value = 1
 var coffee = 0
 var water = 0
-var cupofcoffee = 0
+var hotcoffee = 0
 var money = 0
-var upgraded_once = false
+var boiler_busy = false
+
 
 func _on_button_pressed():
-	bean += 1
-	$bean.text = "Bean: " + str(bean)
-	if bean == 10:
-		bean = 0
+	score_bean += bean_value
+	$bean.text = "Bean: " + str(score_bean)
+	if score_bean >= 10 and water >= 2:
+		score_bean -= 10
+		water -= 2
 		coffee += 1
-		$coffee.text = "coffee: " + str(coffee)
+		$bean.text = "Bean: " + str(score_bean)
+		$water.text = "Water: " + str(water)
+		$coffee.text = "Coffee: " + str(coffee)
 
 
 func _on_timer_timeout():
-	print("Water generated!")
 	water += 1
 	$water.text = "Water: " + str(water)
-	_create_hot_coffee()
 
-func _create_hot_coffee():
-	while coffee >= 10 and water >= 2:
-		coffee -= 10 
-		water -= 2
-		$water.text = "Water: " + str(water)
-		cupofcoffee += 1
-		print("completed")
-		$cupofcoffee.text = "Cupofcoffee: " + str(cupofcoffee)
 
 func _on_sell_coffee_pressed():
-	if cupofcoffee >=1:
-		cupofcoffee -= 1
-		money += 1
-		$cupofcoffee.text = "Cupofcoffee: " + str(cupofcoffee)
+	if hotcoffee >= 1:
+		hotcoffee -= 1
+		money += 5
+		$hotcoffee.text = "Hotcoffee: " + str(hotcoffee)
 		$money.text = "Cash $: " + str(money)
 
 
 func _on_upgrade_water_pressed() -> void:
-	if money >= 10:
-		money -= 10
-		$Timer.wait_time = 8
-		$Timer.start()
-		upgraded_once = true
-		$money.text = "Cash $: " + str(money)
-
-func _on_upgrade_bean_pressed() -> void:
 	if money >= 5:
 		money -= 5
-		bean += 2
-		
-	pass # Replace with function body.
+		$Timer.wait_time = max(0.5, $Timer.wait_time * 0.8)
+		$Timer.stop()
+		$Timer.start()
+		$money.text = "Cash $: " + str(money)
+
+
+func _on_upgrade_bean_pressed() -> void:
+	if money >= 10:
+		money -= 10
+		bean_value += 1
+		$money.text = "Cash $: " + str(money)
+
+
+func start_boiler() -> void:
+	if coffee >= 1 and not boiler_busy:
+		coffee -= 1
+		boiler_busy = true
+		$coffee.text = "Coffee: " + str(coffee)
+		$boiler_timer.wait_time = 20
+		$boiler_timer.start()
+
+
+func _on_boiler_timer_timeout():
+	hotcoffee += 1
+	boiler_busy = false
+	$hotcoffee.text = "Hotcoffee: " + str(hotcoffee)
+	if coffee >= 1:
+		start_boiler()
